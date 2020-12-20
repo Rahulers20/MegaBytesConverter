10bc496d033bfdfb3a59d66e715ba54664c249a1
Rahul Ravishankar <rahulrshankar02@gmail.com>
2020-12-20 21:01:53 +0530

    Initial commit

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,16 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+
+        if(kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+
+            int megaBytes = Math.round(kiloBytes / 1024);
+
+            int remainingKiloBytes = Math.round(kiloBytes % 1024);
+
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingKiloBytes + " KB");
+        }
+    }
+}
